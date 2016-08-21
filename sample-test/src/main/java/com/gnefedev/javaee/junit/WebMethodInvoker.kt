174175856439d82810ec23bef6724025e5ce2f07
gerakln <georgynefedev@gmail.com>
2016-08-21 18:06:27 +0300

    нет соединения

--- sample-test/src/main/java/com/gnefedev/javaee/junit/WebMethodInvoker.kt
+++ sample-test/src/main/java/com/gnefedev/javaee/junit/WebMethodInvoker.kt
@@ -4,6 +4,7 @@ import com.gnefedev.javaee.model.TestResponse
 import com.gnefedev.javaee.model.TestStatus
 import org.junit.runners.model.FrameworkMethod
 import org.junit.runners.model.Statement
+import org.springframework.web.client.HttpClientErrorException
 
 /**
  * Created by gerakln on 21.08.16.
@@ -13,9 +14,14 @@ internal class WebMethodInvoker(val method: FrameworkMethod) : Statement() {
         val className = method.method.declaringClass.name
         val methodName = method.name
         val uri = "http://localhost:8080/test/test/$className/$methodName"
-        val response = RestTemplateForTest.template
-                .getForEntity(uri, TestResponse::class.java)
-                .body
+        val response: TestResponse<*>?
+        try {
+            response = RestTemplateForTest.template
+                           .getForEntity(uri, TestResponse::class.java)
+                           .body
+        } catch(e: HttpClientErrorException) {
+            throw RuntimeException("Не найден продеплоеный тест по адресу $uri")
+        }
         if (response.status == TestStatus.ERROR) {
             throw response.error!!
         }
