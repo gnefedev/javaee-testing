a6526dd06bc04c9b0fe9dd6008ca289a85691fb2
gerakln <georgynefedev@gmail.com>
2016-08-27 22:02:58 +0300

    pom refactor

--- sample-ejb/src/main/java/com/gnefedev/sample/HelloWorld.java
+++ sample-ejb/src/main/java/com/gnefedev/sample/HelloWorld.java
@@ -2,18 +2,12 @@ package com.gnefedev.sample;
 
 import javax.ejb.MessageDriven;
 import javax.inject.Inject;
-import javax.jms.Message;
-import javax.jms.MessageListener;
 
 /**
  * Created by gerakln on 31.07.16.
  */
 @MessageDriven
-public class HelloWorld implements MessageListener {
+public class HelloWorld {
     @Inject
     private FromCore fromCore;
-
-    public void onMessage(Message message) {
-
-    }
 }
